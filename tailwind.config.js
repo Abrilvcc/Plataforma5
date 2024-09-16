9369571110a3274b12ed11f11ad48e2726cf4310
Abril Campana <abril.campana95@hotmail.com>
2024-09-16 16:18:23 -0300

    falta corregir

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customRed: '#752642', // Nombre del color personalizado
+        },
+      },
+    },
+    plugins: [],
+  }
